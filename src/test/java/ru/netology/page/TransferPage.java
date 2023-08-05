ad166d309d445ca1ec05a56a9628dc58f673ed71
Olga <o.n.p@mail.ru>
2023-08-05 18:51:58 +0300

    Add successful tests

---
+++ src/test/java/ru/netology/page/TransferPage.java
@@ -0,0 +1,53 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.data.DataHelper;
+
+import java.time.Duration;
+import java.util.Random;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class TransferPage {
+
+    private final SelenideElement amountInput = $("[data-test-id=amount] input");
+    private final SelenideElement fromInput = $("[data-test-id=from] input");
+    private final SelenideElement transferButton = $("[data-test-id=action-transfer]");
+    private final SelenideElement transferHead = $(byText("Пополнение карты"));
+    private final SelenideElement errorMessageContent = $("[data-test-id='error-notification'] .notification__content");
+    private final SelenideElement errorMessageTitle = $("[data-test-id='error-notification'] .notification__title");
+
+    public TransferPage() {
+        transferHead.shouldBe(Condition.visible);
+    }
+
+    public void makeTransfer(String amountToTransfer, DataHelper.CardInfo cardInfo) {
+        amountInput.setValue(amountToTransfer);
+        fromInput.setValue(cardInfo.getCardNumber());
+        transferButton.click();
+    }
+
+    public void makeTransferError(String amountToTransfer, DataHelper.CardInfo cardInfo) {
+        amountInput.setValue(amountToTransfer);
+        long first15 = (long) (Math.random() * 1000000000000000L);
+        String strFirst15 = Long.toString(first15);
+        String errorCard = "0" + strFirst15;
+        fromInput.setValue(errorCard);
+        transferButton.click();
+    }
+
+    public DashboardPage makeValidTransfer(String amountToTransfer, DataHelper.CardInfo cardInfo) {
+        makeTransfer(amountToTransfer, cardInfo);
+        return new DashboardPage();
+    }
+
+    public void findErrorMessageContent(String expectedText, String title) {
+        errorMessageContent.shouldHave(Condition.exactText(expectedText), Duration.ofSeconds(15)).shouldBe(Condition.visible);
+        errorMessageTitle.shouldHave(Condition.exactText(title), Duration.ofSeconds(15)).shouldBe(Condition.visible);
+    }
+
+
+
+}
---
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -0,0 +1,94 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.page.DashboardPage;
+import ru.netology.page.LoginPage;
+import ru.netology.page.TransferPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.data.DataHelper.*;
+
+public class MoneyTransferTest {
+
+    DashboardPage dashboardPage;
+
+    @BeforeEach
+    void setup() {
+        var loinPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = getAuthInfo();
+        var verificationPage = loinPage.validLogin(authInfo);
+        var verificationCode = getVerificationCode();
+        dashboardPage = verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    void transferFromFirstToSecondCard() {
+        var firstCardInfo = getFirstCard();
+        var secondCardInfo = getSecondCard();
+        var firstCardBalance = dashboardPage.getCardBalance(firstCardInfo);
+        var secondCardBalance = dashboardPage.getCardBalance(secondCardInfo);
+        var amount = generalValidAmount(firstCardBalance);
+        var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo);
+        dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), firstCardInfo);
+
+        var expectedBalanceFirstCard = firstCardBalance - amount;
+        var expectedBalanceSecondCard = secondCardBalance + amount;
+        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+
+        Assertions.assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
+        Assertions.assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
+
+    }
+
+    @Test
+    void transferFromSecondToFirstCard() {
+        var firstCardInfo = getFirstCard();
+        var secondCardInfo = getSecondCard();
+        var firstCardBalance = dashboardPage.getCardBalance(firstCardInfo);
+        var secondCardBalance = dashboardPage.getCardBalance(secondCardInfo);
+        var amount = generalValidAmount(secondCardBalance);
+        var transferPage = dashboardPage.selectCardToTransfer(firstCardInfo);
+        dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), secondCardInfo);
+
+        var expectedBalanceFirstCard = firstCardBalance + amount;
+        var expectedBalanceSecondCard = secondCardBalance - amount;
+        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+
+        Assertions.assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
+        Assertions.assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
+
+    }
+
+    @Test
+    void transferFromFirstToSecondCardInvalidNumberCard() {
+        var firstCardInfo = getFirstCard();
+        var secondCardInfo = getSecondCard();
+        var firstCardBalance = dashboardPage.getCardBalance(firstCardInfo);
+        var amount = generalValidAmount(firstCardBalance);
+        dashboardPage.selectCardToTransfer(secondCardInfo);
+
+        TransferPage error = new TransferPage();
+        error.makeTransferError(String.valueOf(amount), firstCardInfo);
+        error.findErrorMessageContent("Ошибка! Произошла ошибка", "Ошибка");
+
+    }
+
+   /* @Test
+    void transferFromFirstToSecondCardInvalidAmount() {
+        var firstCardInfo = getFirstCard();
+        var secondCardInfo = getSecondCard();
+        var firstCardBalance = dashboardPage.getCardBalance(firstCardInfo);
+        var amount = generalInvalidAmount(firstCardBalance);
+        dashboardPage.selectCardToTransfer(secondCardInfo);
+
+        TransferPage error = new TransferPage();
+        error.makeTransfer(String.valueOf(amount), firstCardInfo);
+        error.findErrorMessageContent("Сумма перевода превышает остаток на карте списания", "Ошибка");
+
+    }*/
+
+}
